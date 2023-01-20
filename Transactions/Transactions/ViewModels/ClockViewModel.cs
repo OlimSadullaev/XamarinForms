244d6746209367eedd797d4e278b2b45239aa559
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-01-20 16:33:35 +0500

    ViewModels

---
+++ Transactions/Transactions/ViewModels/ClockViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Transactions.ViewModels
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            /*Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };*/
+            this.DateTime = DateTime.Now;
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+    }
+}
